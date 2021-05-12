b121e3ebda319ab15cc2d04097ec7e0ac6b88f32
Kerosz <andrei_chirila@outlook.com>
2021-05-12 14:58:45 +0300

    feature: added table component

--- src/pages/s/[id]/routes.tsx
+++ src/pages/s/[id]/routes.tsx
@@ -1,7 +1,36 @@
+// packages
+import useSWR from "swr";
 // components
 import SiteLayout from "@components/layouts/site";
+import Table from "@components/ui/table";
+import Link from "@components/ui/link";
+import Button from "@components/ui/button";
+// types
+import { PageProps, SiteData } from "@/types";
 
-export default function Routes() {
+const columns = [
+  {
+    label: "Path",
+    orderBy: "url",
+  },
+  {
+    label: "Created",
+    orderBy: "created_at",
+  },
+  {
+    label: "View",
+    options: {
+      readerOnly: true,
+    },
+  },
+];
+export default function Routes({ data }: PageProps<SiteData>): JSX.Element {
+  const { data: routeData } = useSWR(data.id ? `/api/routes/${data.id}` : null);
+
+  if (!routeData) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="bg-white-normal shadow overflow-hidden sm:rounded-lg w-full">
       <div className="px-4 py-5 sm:px-6">
@@ -9,7 +38,40 @@ export default function Routes() {
         <p className="mt-1 max-w-2xl text-sm text-gray-500">Information about site routes</p>
       </div>
 
-      <div className="border-t border-b border-gray-200">routes content</div>
+      <div className="border-t border-gray-200">
+        <Table
+          rowData={routeData}
+          columnData={columns}
+          rowsPerPage={5}
+          defaultOrderBy="created_at"
+          withPagination={routeData.length > 5}
+        >
+          <Table.Body>
+            {({ rows }) =>
+              rows.map((entry) => (
+                <Table.Row key={entry.id || entry.name}>
+                  <Table.DataCell>
+                    <Link href={`/s/${entry.id}`} className="font-semibold text-black-normal">
+                      {entry.name}
+                    </Link>
+                  </Table.DataCell>
+                  <Table.DataCell>
+                    <Link href={entry.url} external className="hover:underline">
+                      {entry.url}
+                    </Link>
+                  </Table.DataCell>
+                  <Table.DataCell fixedWidth>{entry.description}</Table.DataCell>
+                  <Table.DataCell>
+                    <Button as={Link} href={`/s/${entry.id}`} variant="secondary" size="small">
+                      Details
+                    </Button>
+                  </Table.DataCell>
+                </Table.Row>
+              ))
+            }
+          </Table.Body>
+        </Table>
+      </div>
     </div>
   );
 }
