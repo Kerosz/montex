90ff08b048e6158c6283a857086cfcbc147fddc8
Kerosz <andrei_chirila@outlook.com>
2021-05-20 23:31:47 +0300

    hotfix: issue with cookie banner not hiding on route change after cookies were accepted was resolved

--- src/components/cookie-banner.tsx
+++ src/components/cookie-banner.tsx
@@ -1,4 +1,5 @@
 // packages
+import cn from "classnames";
 import Cookie from "js-cookie";
 import { useState } from "react";
 // components
@@ -6,29 +7,35 @@ import Container from "@components/ui/container";
 import Link from "@components/ui/link";
 import Button from "@components/ui/button";
 import Switch from "@components/ui/switch";
-import cn from "classnames";
 
-const COOKIE_KEY = "__cookie_consent";
+/**
+ * Getting the default state for the banner from the cookies
+ * If cookie returns a value we want to negate it in order to not show the banner
+ */
+const COOKIE_KEY = "__cpk_consent";
 const DEFAULT_SHOW_STATE = !Cookie.get(COOKIE_KEY);
 
 export default function CookieBanner() {
   const [showState, setShow] = useState(DEFAULT_SHOW_STATE);
 
   // Toggle switch states
-  const [basicState] = useState<boolean>(true);
+  const [necessaryState] = useState<boolean>(true);
   const [analyticsState, setAnalytics] = useState<boolean>(true);
   const [marketingState, setMarketing] = useState<boolean>(false);
 
   const handleCookieConsent = (type?: "all") => {
     const consentSettings = {
-      basic: type === "all" ? true : basicState,
+      necessary: type === "all" ? true : necessaryState,
       analytics: type === "all" ? true : analyticsState,
       marketing: type === "all" ? true : marketingState,
     };
 
-    Cookie.set(COOKIE_KEY, JSON.stringify(consentSettings), { expires: 365 });
+    Cookie.set(COOKIE_KEY, JSON.stringify(consentSettings), { expires: 365, secure: true });
 
     setShow(false);
+
+    // Need to reload the page to get the new cookie settings
+    window.location.reload();
   };
 
   const rootClass = cn(
@@ -44,7 +51,7 @@ export default function CookieBanner() {
       <Container className="flex flex-col md:flex-row">
         <div className="pr-4 text-gray-700 flex-grow">
           <p className="pb-5">
-            This website stores data such as cookies to enable necessary basic site functionality,
+            This website stores data such as cookies to enable necessary site functionality,
             including optional analytics and targeting. You may change your settings at any time or
             accept the default settings.{" "}
             <Link href="/legal/privacy-policy" className="text-secondary hover:underline" external>
@@ -53,7 +60,7 @@ export default function CookieBanner() {
           </p>
 
           <div className="flex flex-col sm:flex-row sm:space-x-6 space-y-4 sm:space-y-0">
-            <Switch label="Basic" isChecked={basicState} setChecked={() => null} />
+            <Switch label="Necessary" isChecked={necessaryState} setChecked={() => null} />
             <Switch label="Analytics" isChecked={analyticsState} setChecked={setAnalytics} />
             <Switch label="Marketing" isChecked={marketingState} setChecked={setMarketing} />
           </div>
